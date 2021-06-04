1c810aa8da5f0dda3c39c96aa19ee2eff4ec29c2
CarlosSarmiento5 <carlosrokylukycsc@hotmail.com>
2021-06-03 22:13:22 -0500

    Add files via upload

---
+++ actormodel/src/ActorModel/actores/Espadachin.java
@@ -0,0 +1,30 @@
+package ActorModel.actores;
+
+
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import ActorModel.Main;
+
+public class Espadachin extends UntypedActor {
+    public enum Mensaje {
+        ESPADA_NUEVA,
+        ESPADA_ROTA
+    }
+
+    private final LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+
+    @Override
+    public void onReceive(Object o) {
+        log.info("[Espadachin] ha recibido el mensaje: \"{}\".", o);
+
+        if (o == Mensaje.ESPADA_ROTA) {
+            Main.herrero.tell(Herrero.Mensaje.CREAR_ESPADA, getSelf());
+        } else if (o == Mensaje.ESPADA_NUEVA) {
+            getContext().stop(getSelf());
+        } else {
+            unhandled(o);
+        }
+    }
+}
+
